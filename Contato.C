236830fb9ea3e03ae1c4a09d59d718b96ba07a9a
ThiagoAraujoR <68234556+ThiagoAraujoR@users.noreply.github.com>
2021-03-19 19:33:26 -0300

    Update Contato.C

--- Contato.C
+++ Contato.C
@@ -1,43 +1,108 @@
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
 
-struct st_contato{
-	char nome[100];
-	char telefone[12];
-	char email[100];
-	char instagram[40];
-};
-
-struct st_agenda{
-	struct st_contato contatos[10];
-}agenda;
-
-int main(){
-	for (int i=0;i<10;i++){
-		printf("Insira o nome: \n");
-		fflush(stdout);
-		fgets(agenda.contatos[i].nome,100,stdin);
-
-		printf("Insira o telefone com DDD\n");
-		fflush(stdout);
-		fgets(agenda.contatos[i].telefone,12,stdin);
+typedef struct{
+	int codigo;
+	int ddd;
+	int numero;
+	char nome[30];
+	char email[50];
+}Contato;
 
 
-		printf("Insira o email\n");
-		fflush(stdout);
-		fgets(agenda.contatos[i].email,100,stdin);
+void InserirContato();
+void ListarContato();
+void ExcluirContato();
+void Menu();
 
-		printf("Insira o instagram:\n");
-		fflush(stdout);
-		fgets(agenda.contatos[i].instagram,40,stdin);
-	}
+static Contato contato[10];
+static int contador_Contato=0;
 
-	printf("=====Agenda de Contatos=====\n");
-	for (int i=0;i<10;i++){
-		printf("===== Contato%d =====",(i+1));
-		printf("Nome:%s \n",strtok(agenda.contatos[i].nome,"\n"));
-		printf("Telefone:%s \n",strtok(agenda.contatos[i].telefone,"\n"));
-		printf("Email:%s \n",strtok(agenda.contatos[i].email,"\n"));
-		printf("Instagram:%s \n",strtok(agenda.contatos[i].instagram,"\n"));
-	}
+void Info(Contato cont){
+	printf("Código:%d \nNome:%s \nNumero:(%d) %d \nEmail:%s\n",
+			cont.codigo,strtok(cont.nome,"\n"),cont.ddd,cont.numero,strtok(cont.email,"\n"));
+}
+int main() {
+	Menu();
 	return 0;
+}
+
+void Menu(){
+	printf("====Olá, eu serei sua agenda====\n");
+	printf("Selecione a opção desejada\n");
+	printf("1-Inserir Contato\n");
+	printf("2-Listar Contatos\n");
+	printf("3-Sair\n");
+	printf("------------------\n");
+
+	int opcao;
+	fflush(stdout);
+	scanf("%d",&opcao);
+	getchar();
+
+	switch (opcao) {
+		case 1:
+			InserirContato();
+			break;
+		case 2:
+			ListarContato();
+			break;
+		case 3:
+			printf("Volte Sempre");
+			Sleep(2);
+			exit(0);
+		default:
+			printf("Opção inexistente\n");
+			Sleep(2);
+			Menu();
+			break;
+	}
+}
+
+void InserirContato(){
+	printf("Adicionar Contato\n");
+	printf("-----------------\n");
+
+	printf("Informe o Nome:");
+	fflush(stdout);
+	fgets(contato[contador_Contato].nome,30,stdin);
+
+	printf("Informe o Email:");
+	fflush(stdout);
+	fgets(contato[contador_Contato].email,50,stdin);
+
+	printf("Informe o DDD:");
+	fflush(stdout);
+	scanf("%d",&contato[contador_Contato].ddd);
+
+	printf("Informe o numero de telefone:");
+	fflush(stdout);
+	scanf("%d",&contato[contador_Contato].numero);
+
+	printf("O contato %s foi adicionado com sucesso\n",strtok(contato[contador_Contato].nome,"\n"));
+	contato[contador_Contato].codigo= (contador_Contato+1);
+	contador_Contato++;
+
+	Sleep(2);
+	Menu();
+}
+
+void ListarContato(){
+	if(contador_Contato>0){
+		printf("---Lista de Contatos---\n");
+		printf("-----------------------\n");
+		for(int i=0;i<contador_Contato;i++){
+			Info(contato[i]);
+			printf("----------\n");
+			Sleep(1);
+		}
+		Sleep(2);
+		Menu();
+	}else{
+		printf("Nenhum contato adicionado");
+		Sleep(2);
+		Menu();
+	}
 }
